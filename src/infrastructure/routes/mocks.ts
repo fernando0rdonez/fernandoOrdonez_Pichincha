cf8d5b63cf80b5e77cdac7d254210f967306b730
Fernando Ordonez <ferjordo2011@gmail.com>
2022-07-17 11:05:11 -0500

    Change logic of mock api

--- src/infrastructure/routes/mocks.ts
+++ src/infrastructure/routes/mocks.ts
@@ -1,11 +1,19 @@
 import express, { Response } from 'express'
+import { Repository } from '../../use-cases/interfaces/repository'
 import useCases from '../../use-cases/mocks'
+import { BadRequestError } from '../errors/bad-request-error'
+import generateDTO from '../common/dto/'
 
 const router = express.Router()
 
-router.get('/', (_, res: Response) => {
-  const listMock = useCases.listRepository()
-  res.json(listMock)
+router.get('/', async (_, res: Response) => {
+  const useCaseResponse = await useCases.listRepository()
+  if (useCaseResponse.error) {
+    throw new BadRequestError(useCaseResponse.data.message)
+  }
+  const repositories = useCaseResponse.data as Repository[]
+  const response = generateDTO.mock.makeDTOMock(repositories)
+  res.json(response)
 })
 
 export { router as mockRouter }
